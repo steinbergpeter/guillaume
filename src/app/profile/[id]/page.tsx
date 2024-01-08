e5cbeb6c5c0c6ade929f19cd3c8e403f0e00657d
Peter <bklynpeter@gmail.com>
2024-01-08 13:28:11 -0500

    replaced useRouter with Links

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 
 export default function ProfileID() {
     const { id } = useParams()
+    const { back, replace, refresh, forward, push, prefetch } = useRouter()
     return (
         <main className="">
             <p>ID Page of my application: {id}</p>
+            <p onClick={() => back()}>back to profile selection page</p>
+            <p onClick={() => push('/')}>back home</p>
         </main>
     )
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,20 +1,31 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export default function Profile() {
-    const { push } = useRouter()
-    // console.log({ router })
-
     return (
         <main className="">
             <p>Profile Page of my application</p>
             <p>My List of Profiles</p>
-            <ul>
-                <li onClick={() => push('/profile/1')}>Profile 1</li>
-                <li onClick={() => push('/profile/2')}>Profile 2</li>
-                <li onClick={() => push('/profile/3')}>Profile 3</li>
-            </ul>
+
+            <div className="flex flex-col gap-2 mt-4">
+                <Link
+                    href="/profile/1"
+                    className="text-blue-200 hover:text-blue-400"
+                >
+                    Profile 1
+                </Link>
+                <Link
+                    href="/profile/2"
+                    className="text-blue-200 hover:text-blue-400"
+                >
+                    Profile 2
+                </Link>
+                <Link
+                    href="/profile/3"
+                    className="text-blue-200 hover:text-blue-400"
+                >
+                    Profile 3
+                </Link>
+            </div>
         </main>
     )
 }
